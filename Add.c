916ff60e7256163178b470c4aaf1cf841d57646f
Santosh <san.enchantrus@yahoo.com>
2017-03-29 12:59:18 +0530

    Program to add to integer numbers

---
+++ Add.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+
+int
+main()
+{
+	int a,b;
+	printf(\n"Enter the value of a \n");
+	scanf("%d", &a);
+	printf(\n"Enter the value of b \n");
+	scanf("\n%d", &b);
+        printf("\n Sum of two numbers is %d\",a+b);
+	return 0;
+}
