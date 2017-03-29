a8586c5ec309b4f1bcba96d32a6b9757a1b857e2
Santosh <san.enchantrus@yahoo.com>
2017-03-29 13:06:24 +0530

    Program to find product of two numbers

---
+++ Product.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+
+int
+main()
+{
+	int a,b;
+	printf(\n"Enter the value of a \n");
+	scanf("%d", &a);
+	printf(\n"Enter the value of b \n");
+	scanf("\n%d", &b);
+	printf(\n"Enter the value of a \n");
+        printf("\n Product of two numbers is %d\",a*b);
+	return 0;
+}
